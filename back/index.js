abd786ec492315fec9b9a0e011b5d8390f8ec8a0
yeray <yeray.programacion.web@gmail.com>
2024-03-01 11:24:29 +0000

    endpoint-getAllplays

--- back/index.js
+++ back/index.js
@@ -1,19 +1,29 @@
 const express = require("express"); 
-const app = express(); 
+const app = express();
+const playRoutes = require ('./src/api/routes/play.routes.js')
+const cors = require("cors");
 
 const { connect } = require ('./bd')
 
-
 connect();
 
+//
+app.use(
+     cors({
+          origin: "*",
+          credential: true,
+     })
+);
+
 app.use(express.json());
+
+app.use("/play", playRoutes);// ruta inicial
+// app.use("/companies", companiesRoutes);// ruta inicial
 
 
 app.get("/", (req, res) => {
      res.send("Express on Vercel"); 
 }); 
-
-
 
 
 const PORT = process.env.PORT || 5002; 
