5fdcb66fe7310d5447a39adf786a0c8462626a6c
yeray <yeray.programacion.web@gmail.com>
2024-03-18 21:42:09 +0000

    mailer config email ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express"); 
+const express = require("express");
 const app = express();
-const playRoutes = require ('./src/api/routes/play.routes.js')
+const playRoutes = require('./src/api/routes/play.routes.js')
 const companyRoutes = require('./src/api/routes/company.routes.js')
 const cors = require("cors");
 const dotenv = require("dotenv").config();
@@ -11,9 +11,9 @@ cloudinary.config({
      api_key: process.env.API_KEY,
      api_secret: process.env.API_SECRET,
      secure: true,
-   });
+});
 
-const { connect } = require ('./bd')
+const { connect } = require('./bd')
 
 connect();
 
@@ -31,11 +31,11 @@ app.use("/companies", companyRoutes);// ruta inicial
 
 
 app.get("/", (req, res) => {
-     res.send("Express on Vercel"); 
-}); 
+     res.send("Express on Vercel");
+});
 
 
-const PORT = process.env.PORT || 5002; 
+const PORT = process.env.PORT || 5003;
 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
--- src/api/utils/mailer.config.js
+++ src/api/utils/mailer.config.js
@@ -1,31 +1,40 @@
 // Importa la biblioteca nodemailer
 const nodemailer = require("nodemailer");
+const dotenv = require("dotenv").config();
+// require('dotenv').config();
+
 
 // Obtiene la contraseña y la dirección de correo electrónico del entorno que estará en el .env
-// const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
-// const EMAIL_MAIL = process.env.EMAIL_MAIL;
+const EMAIL_MAIL = process.env.EMAIL_MAIL;
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
 
 // Crea un objeto de transporte utilizando la configuración proporcionada
 const transporter = nodemailer.createTransport({
     service: "gmail", // Utiliza el servicio Gmail para enviar correos electrónicos
     auth: {
-        user: "ensayomailer@gmail.com", // Dirección de correo electrónico del remitente
-        pass: "xplibpqiexgwewih", // Contraseña de la dirección de correo electrónico del remitente
+        user: EMAIL_MAIL, // Dirección de correo electrónico del remitente
+        pass: EMAIL_PASSWORD, // Contraseña de la dirección de correo electrónico del remitente
     },
     tls: { rejectUnauthorized: false }, // Permite conexiones TLS sin autenticación
 });
+console.log(EMAIL_MAIL)
 
 // Exporta una función para enviar correos electrónicos de recibos
 module.exports.sendEmail = () => {
     transporter // Utiliza el objeto de transporte para enviar el correo electrónico
         .sendMail({
-            from: "ensayomailer@gmail.com", // Dirección de correo electrónico del remitente
+            from: EMAIL_MAIL, // Dirección de correo electrónico del remitente
             to: "yeray.dtc@gmail.com", // Dirección de correo electrónico del destinatario (usuario)
-            subject: `Bienvenido a AGO`, // Asunto del correo electrónico
+            subject: ` Reserva no confirmada.`, // Asunto del correo electrónico
             html: `
-        <h3>Bienvenido a AGO, tu plataforma para encontrar todo lo que necesitas para tener una vida cómoda</h3>
-        <p>Confirma tu mail pulsando el siguiente enlace </p>
-        <p> Por favor, pulsa aquí para acceder a la cuenta premium  <button> Premium</button> </p>
+                <body style="font-family: Arial, sans-serif;">
+                <h1 style="color: #333; font-size: 24px; margin-bottom: 20px;">Confirmación de reserva de entradas</h1>
+                <p style="color: #333; font-size: 16px;">Se ha enviado un correo electrónico con su solicitud de entradas para la obra <strong>xxxx</strong> el día <strong>tal</strong> a las <strong>xxx</strong>. La reserva no está confirmada hasta que la asociación no se ponga en contacto a través de los medios indicados.</p>
+                <p style="color: #333; font-size: 16px;">Si en unos días no recibe respuesta, por favor, póngase en contacto con ellos a través de <strong>{correo asociación}</strong> o <strong>{teléfono}</strong>.</p>
+                <p style="color: #333; font-size: 16px;">Por favor, no realice una nueva reserva para evitar duplicados en las solicitudes.</p>
+                <p style="color: #333; font-size: 16px;">¡Gracias!</p>
+                <p style="color: #333; font-size: 16px;">¡Que disfrute de la obra!</p>
+                </body>
             ` // Contenido HTML del correo electrónico
         })
         .then(() => { // Maneja el caso en que el correo electrónico se envíe correctamente
